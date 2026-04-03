b6d46801a81e82a0958f74013ef43fd9b11de044
kmsmahi <salauddinmahi2002@gmail.com>
2026-04-03 10:23:25 +0600

    done.......

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const User = ({user}) => {
     const {name,email,phone}=user;
+    const navigate = useNavigate();
+    const handleNavigate=()=>{
+        navigate('/mobiles');
+    }
     return (
         <div className='border-2 border-green-500 p-4 font-semibold'>
             <h3>{name}</h3>
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
             <Link className='p-2 text-blue-500 hover:text-blue-700' to={`/users/${user.id}`}>View Details</Link>
+            <button onClick={handleNavigate} className='p-2 bg-blue-500 text-white hover:bg-blue-700'>
+                Details Num: {user.id}
+            </button>
         </div>
     );
 };
